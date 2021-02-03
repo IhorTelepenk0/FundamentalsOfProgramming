8936a006604e9f570ce4a9680ac20d971ff1a0f5
IgorTelepenko <telepenko.igor2@ukr.net>
2021-02-03 08:07:46 +0200

    lab3 tasks

---
+++ Lab3 tasks/Part1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for testing
+        }
+
+        public static void Exercise1()
+        {
+            Console.WriteLine("enter the 3 numbers");
+            int[] arr = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            var min = arr[0];
+            var max = arr[0];
+
+            for (int i = 1; i < 3; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+
+            Console.WriteLine("the gratest: " + max);
+            Console.WriteLine("the smallest: " + min);
+        }
+
+        public static void Exercise2()
+        {
+            Console.WriteLine("enter the 3 grades");
+            int[] arr = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            var sum = 0;
+
+            for (int i = 1; i < 3; i++)
+            {
+                sum += arr[i];
+            }
+
+            Console.WriteLine("the average grade: " + sum/3);
+        }
+
+        public static void Exercise3()
+        {
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine("the number is positive");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine("the number is negative");
+            }
+            else
+            {
+                Console.WriteLine("the number is 0");
+            }
+        }
+
+        public static void Exercise4()
+        {
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("the number is even");
+            }
+            else
+            {
+                Console.WriteLine("the number is odd");
+            }
+        }
+
+        public static void Exercise5()
+        {
+            var ch = Console.ReadLine();
+
+            if (ch.Length > 1)
+            {
+                Console.WriteLine("it is a string, not a character");
+            }
+            else
+            {
+                Console.WriteLine("it is a character");
+            }
+        }
+
+        public static void Exercise6()
+        {
+            var mon = Convert.ToInt32(Console.ReadLine());
+            if (mon > 0 && mon <= 12)
+            {
+                Console.WriteLine("the number of days is: " + DateTime.DaysInMonth(DateTime.Now.Year, mon));
+            }
+
+        }
+    }
+}
---
+++ Lab3 tasks/Part2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for testing
+        }
+
+        public static void Exercise1()
+        {
+            var a = Console.ReadLine();
+            string digits = "0123456789";
+
+            if (a.Contains(a))
+                Console.WriteLine(a);
+            else
+                Console.WriteLine("not allowed");
+        }
+
+        public static void Exercise2()
+        {
+            Console.WriteLine("enter the operation (by symbol): ");
+            var op = Console.ReadLine();
+            Console.WriteLine("enter the first number: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the second number: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(a + b);
+                    break;
+
+                case "-":
+                    Console.WriteLine(a - b);
+                    break;
+
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+
+                case "/":
+                    Console.WriteLine(a / b);
+                    break;
+
+                default:
+                    Console.WriteLine("wrong operation");
+                    break;
+            }
+        }
+
+        public static void Exercise3()
+        {
+            Console.WriteLine("enter the binary number: ");
+            var bin = Console.ReadLine();
+            var hex = Convert.ToInt32(bin, 2).ToString("X");
+
+            Console.WriteLine(hex);
+        }
+
+        public static void Exercise4()
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+                Console.WriteLine("the number is perfect");
+            else
+                Console.WriteLine("the number isn't perfect");
+        }
+
+        public static void Exercise5()
+        {
+            Console.WriteLine("enter the number: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n + "1");
+            for (var i = 0; n % 2 == 0; n /= 2)
+            {
+                Console.Write(" * 2");
+            }
+            for (var i = 3; i <= n;)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(" * " + i);
+                    n /= i;
+                }
+                else
+                {
+                    i += 2;
+                }
+            }
+        }
+    }
+}
