1c36d2f108eca93e397b3c227be4e5131ff34578
IgorTelepenko <telepenko.igor2@ukr.net>
2021-02-03 06:52:27 +0200

    Добавьте файлы проекта.

---
+++ Task1name/task3dividing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task3dividing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, dev;
+            n1 = Convert.ToDouble(Console.ReadLine());
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            dev = n1 / n2;
+            Console.Write(dev);
+        }
+    }
+}
