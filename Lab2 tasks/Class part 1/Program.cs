e778fabaab33a961bbc6d6ca1536eaf2159a8d0d
IgorTelepenko <telepenko.igor2@ukr.net>
2021-02-03 07:36:44 +0200

    Lab2 tasks

---
+++ Lab2 tasks/Class part 1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Class_part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for testing
+        }
+
+        public static void Exercise1()
+        {
+            double n1, n2, res;
+            n1 = Convert.ToDouble(Console.ReadLine());
+            n2 = Convert.ToDouble(Console.ReadLine());
+            string operation = Console.ReadLine();
+
+            if (operation == "adding")
+                res = n1 + n2;
+            else
+            {
+                if (operation == "subtracting")
+                    res = n1 - n2;
+                else
+                {
+                    if (operation == "multiplying")
+                        res = n1 * n2;
+                    else
+                    {
+                        if (operation == "dividing")
+                            res = n1 / n2;
+                        else
+                        {
+                            Console.WriteLine("wrong operation");
+                            res = 0;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(res);
+        }
+
+        public static void Exercise2()
+        {
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{a} x {i} = {a * i}");
+            }
+        }
+
+        public static bool Exercise3()
+        {
+            Console.WriteLine("enter the first number");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a < 0 && b > 0)
+            {
+                return true;
+            }
+            else if (b < 0 && a > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Exercise4()
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            if (a >= 20 && a <= 100)
+                Console.WriteLine("the number is between 20 and 100");
+            else
+                Console.WriteLine("the number is not between 20 and 100");
+        }
+
+        public static void Exercise5()
+        {
+            for (int i = 1; i < 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Lab2 tasks/class part 2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace class_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for testing
+        }
+
+        public static void Exercise1()
+        {
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 7 == 0 || a % 3 == 0)
+            {
+                Console.WriteLine("This number is a multiple of 7 or 3");
+            }
+            else
+            {
+                Console.WriteLine("This number is not a multiple of 7 or 3");
+            }
+        }
+
+        public static void Exercise2()
+        {
+            Console.WriteLine("enter number 1: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 2: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            if ((a > -10 && a < 10) || (b > -10 && b < 10))
+            {
+                Console.WriteLine("there is an integer between 10 and -10");
+            }
+            else
+            {
+                Console.WriteLine("No number between 10 and -10");
+            }
+        }
+
+        public static void Exercise3()
+        {
+            Console.WriteLine("enter number 1: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 2: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            if (a < 100 && b > 200)
+            {
+                Console.WriteLine("one of the numbers is less than 100 and the other is greater than 200");
+            }
+            else
+            {
+                Console.WriteLine("one of the numbers is not less than 100 and the other is not greater than 200");
+            }
+        }
+
+        public static void Exercise4()
+        {
+            Console.WriteLine("enter the radius: ");
+            var a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Area: {Math.PI * a * a}; Perimeter: {2 * Math.PI * a}");
+        }
+
+        public static void Exercise5()
+        {
+            Console.WriteLine("enter the distance: ");
+            var a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the time: ");
+            var b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Speed: {a / b} (km/h), Speed: { (a / b) / 1.609} (mph)");
+        }
+
+        public static void Exercise6()
+        {
+            Console.WriteLine("Enter radius: ");
+            var rad = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine($"Surface: {4 * Math.PI * rad * rad}; Volume: { (4 / 3) * Math.PI * rad * rad * rad}");
+        }
+
+        public static bool Exercise7()
+        {
+            var a = Convert.ToDouble(Console.ReadLine());
+            var b = Convert.ToDouble(Console.ReadLine());
+
+            if (a % 2 == 0 && b % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
