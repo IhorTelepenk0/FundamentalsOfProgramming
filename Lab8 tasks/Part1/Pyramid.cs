6690a38359d5a78987298e2e38f0637b80fd4537
IgorTelepenko <telepenko.igor2@ukr.net>
2021-02-06 19:51:26 +0200

    lab8 tasks

---
+++ Lab8 tasks/Part1/Pyramid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part1
+{
+    class Pyramid
+    {
+        public Pyramid(int size)
+        {
+            for (int i = 0; i <= size; i++)
+            {
+                for (int j = 1; j <= size - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ Lab8 tasks/Part2/Floyd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part2
+{
+    class Floyd
+    {
+        public Floyd(int n)
+        {
+            var v = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(v + " ");
+                    v++;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
