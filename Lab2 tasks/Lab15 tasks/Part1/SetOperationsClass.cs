26609c58475b0c9d3a57ad899265a658137c7276
IgorTelepenko <telepenko.igor2@ukr.net>
2021-02-08 19:12:18 +0200

    lab15 tasks

---
+++ Lab2 tasks/Lab15 tasks/Part1/SetOperationsClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Part1
+{
+    class SetOperationsClass<T>
+    {
+        private T[] set1;
+        private T[] set2;
+
+        public SetOperationsClass(T[] setA, T[] setB)
+        {
+            set1 = setA;
+            set2 = setB;
+        }
+
+        IEnumerable<T> SetsUnion()
+        {
+            IEnumerable<T> res = from el in set1.Union(set2)
+                select el;
+            return res;
+        }
+
+        IEnumerable<T> SetsIntersection()
+        {
+            IEnumerable<T> res = from el in set1.Intersect(set2)
+                select el;
+            return res;
+        }
+
+        IEnumerable<T> SetsDiff()
+        {
+            IEnumerable<T> res = from el in set1.Except(set2)
+                select el;
+            return res;
+        }
+    }
+}
---
+++ Lab2 tasks/Lab15 tasks/Part2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the real number: ");
+            double numb = 0;
+
+            try
+            {
+                numb = Convert.ToDouble(Console.ReadLine());
+                double res;
+                res = Math.Sqrt(numb);
+                Console.WriteLine("the result is " + res);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("the number is written in a wrong way!");
+            }
+
+            
+
+        }
+    }
+}
